b8f2f265b90481ff3ea39010c315c6fd81d38331
Johnson Sebire <johnson@manifestghana.com>
2022-04-23 19:52:32 -0700

    Added Student Admission Livewire Component

---
+++ app/Http/Livewire/StudentAdmission.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class StudentAdmission extends Component
+{
+    public function render()
+    {
+        return view('livewire.student-admission');
+    }
+}
